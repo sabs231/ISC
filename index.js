ec90eff9da28d24c1acd7bc41e61096e510fef75
Sergio Baez <sergio.baez231@gmail.com>
2013-10-31 13:02:07 -0600

    added bootstrap

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ var express = require('express');
 var app = express();
 
 app.configure(function () {
-	app.set('view engine', 'jade');
-	app.set('views', __dirname + '/views');
+	//app.set('view engine', 'jade');
+	//app.set('views', __dirname + '/views');
 	app.use(express.logger());
 	app.use(express.bodyParser());
 	app.use(express.cookieParser());
-	app.use(express.static(__dirname + '/scripts'));
-	app.use(express.static(__dirname + '/css'));
-	app.use(express.static(__dirname + '/img'));
+	app.use(express.static(__dirname + '/views'));
+	app.use('/scripts', express.static(__dirname + '/scripts'));
+	app.use('/css', express.static(__dirname + '/css'));
+	app.use('/img', express.static(__dirname + '/img'));
+	app.use('/fonts', express.static(__dirname + '/fonts'));
 	app.use(app.router);
 });
 
 app.get("/", function(request, response){
-	response.render("index", { message : "THIS IS ISC!"});
+	//response.render("index", { message : "THIS IS ISC!", pageTitle: "Ingeniero en Sistemas Computacionales"});
+	response.sendfile('index.html');
 });
 
 app.listen(8888);
